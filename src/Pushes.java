ec211b97ef0fed93adfbb19317ad2d96d775a8e0
Orilow <ilyaorlov1998@gmail.com>
2020-02-09 21:41:30 +0500

    Start doing second task

---
+++ src/Pushes.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Pushes {
+    public static void main(String[] args) {
+        BufferedReader bi =  new BufferedReader(new InputStreamReader(System.in));
+    }
+}
+
+final class Settings {
+    public static Long time;
+    public static Integer age;
+    public static char gender;
+    public static Integer os_version;
+    public static float x_coord;
+    public static float y_coord;
+
+    private Settings() {}
+    public static void parse(String input) {
+
+    }
+}
